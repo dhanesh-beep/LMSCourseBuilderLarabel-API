f1488b66ee8acfdff8e7b159c35d39a8fd24d80b
Dhanesh Yogi <dhanesh@eligocs.com>
2026-03-16 10:36:42 +0530

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfExtractController;
 use App\Http\Controllers\UserLearningChatbotController;
 use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\HSE_data_upsert;
 use App\Http\Controllers\PineconeController;
-
 
 # Course and Quiz Builder
 Route::post('/extract', [PdfExtractController::class, 'chatbotCourseBuilder']);
@@ -15,16 +15,24 @@ Route::post('/extract', [PdfExtractController::class, 'chatbotCourseBuilder']);
 Route::get('/pinecone/init', [PineconeController::class, 'initIndex']);
 Route::post('/pinecone/upsert', [PineconeController::class, 'upsert']);
 Route::post('/pinecone/update', [PineconeController::class, 'update']);
+
 // Route::post('/pinecone/delete/ids', [PineconeController::class, 'deleteByIds']);
 Route::post('/pinecone/delete/course', [PineconeController::class, 'deleteByCourse']);
 Route::post('/pinecone/query', [PineconeController::class, 'query']);
-
-# User Learning Chatbot - Flowise
-Route::post('/userQueryReply', [UserLearningChatbotController::class, 'userQueryReply']);
 
 # User Learning Chatbot - n8n
 Route::post('/chatbot', [UserLearningChatbotController::class, 'n8nChatbot']);
 
 # Pinecone - Extract Course Text form json file
-Route::post('/pinecone/extract-course-text', [PineconeController::class, 'extractCourseText']);
+// Route::post('/pinecone/extract-course-text', [PineconeController::class, 'extractCourseText']);
 
+# Auto HSE data Upsert API
+Route::post('/pinecone/auto-hsedata-upsert', [UserLearningChatbotController::class, 'n8nChatbot']);
+
+# Extract HSE Data from Web
+Route::post('/hse/pipeline', [HSE_data_upsert::class, 'runFullPipeline']);    
+
+// Route::get('/hse/extract-hse-data', [HSE_data_upsert::class, 'extractHSEData']);       
+// Route::get('/hse/extract', [HSE_data_upsert::class, 'extractHSEData']);
+// Route::post('/hse/upsert', [HSE_data_upsert::class, 'upsertToPinecone']);
+// Route::post('/hse/init-index', [HSE_data_upsert::class, 'initIndex']);
